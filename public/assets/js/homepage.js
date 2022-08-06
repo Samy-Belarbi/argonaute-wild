c64ebceeae7deb2c69a45a2ae9c69512340497cf
Samy <samy.belarbi@3wa.io>
2022-08-06 13:56:38 +0200

    option checked bg

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -26,6 +26,7 @@ const checkForm = async (e) => {
     e.preventDefault();
     const input = document.getElementById("argonaute-name");
     deleteAllErrors();
+    checkSelect(select.value);
 
     if (checkInput(input.value) && checkSelect(select.value)) {
 
